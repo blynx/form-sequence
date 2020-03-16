42643d5f5194e24c00c178db824eb259e0aa7d59
Steffen <mail@steffenhenschel.com>
2020-03-16 16:28:33 +0100

    new query in html string utility

--- test/utilities.test.js
+++ test/utilities.test.js
@@ -1,6 +1,8 @@
+require('browser-env')()
 let test = require("ava")
 
 let nameOrId = require("../lib/utilities.js").nameOrId
+let queryInHtmlString = require("../lib/utilities.js").queryInHtmlString
 
 test("utility: nameOrId()", t => {
   let name = "pete"
@@ -8,4 +10,9 @@ test("utility: nameOrId()", t => {
   let result = nameOrId(name)
 
   t.is(result, expected, "builds a correct query string")
+})
+
+test("utility: queryInHtmlString()", t => {
+  let node = queryInHtmlString("<html><head><title>bye</title></head><body><div><p>yep</p></div></body></html>", "p")
+  t.is(node.textContent, "yep", "queries a single node in a string of html")
 })
