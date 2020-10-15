c7606744f25b9063a15ccae56ed79f9cd940132f
Steffen <mail@steffenhenschel.com>
2020-10-15 10:12:37 +0200

    new start event

--- demo.js
+++ demo.js
@@ -9,9 +9,11 @@ configure({
 document.addEventListener("DOMContentLoaded", () => {
   customElements.define("form-sequence", FormSequence)
   Array.from(document.querySelectorAll("form-sequence")).forEach(element => {
+    element.addEventListener("start", e => console.log("start triggered", e.target))
     element.addEventListener("done", e => console.log("done triggered", e.target))
     element.addEventListener("success", e => console.log("success triggered", e.target))
     element.addEventListener("error", e => console.log("error triggered", e.target))
+    element.addEventListener("return", e => console.log("return triggered", e.target))
 
     element.addEventListener("return", e => {
       alert("Form sequence returned. Will do page reload!")
