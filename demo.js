ef2236e66c8528da8052c8e4fa42caaed112582d
Steffen <mail@steffenhenschel.com>
2020-11-13 17:39:55 +0100

    new cancel event

--- demo.js
+++ demo.js
@@ -14,10 +14,17 @@ document.addEventListener("DOMContentLoaded", () => {
     element.addEventListener("success", e => console.log("success triggered", e.target))
     element.addEventListener("error", e => console.log("error triggered", e.target, e.detail))
     element.addEventListener("return", e => console.log("return triggered", e.target))
+    element.addEventListener("cancel", e => console.log("cancel triggered", e.target))
 
     element.addEventListener("return", e => {
       alert("Form sequence returned. Will do page reload!")
       window.location.href = e.detail.url.href
+    })
+
+    element.addEventListener("cancel", e => {
+      console.log("cancel --->", e)
+      alert("Form sequence cancelled. Will do page reload!")
+      window.location.href = e.detail.homeUrl.href
     })
   })
 })
